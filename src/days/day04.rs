8707962cdf057ab9ec099728afeab38074d2362a
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2024-12-04 22:41:22 +0000

    added container to avoid subslicing lines

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,8 +1,8 @@
 use crate::{DayResult, IntoDayResult};
-use anyhow::Result;
+use anyhow::{bail, Result};
 
 pub fn solve(input: &str) -> Result<DayResult> {
-    let lines = input.lines().map(str::as_bytes).collect::<Vec<_>>();
+    let lines = Container::new(input)?;
 
     let p1 = solve_p1(&lines);
     let p2 = solve_p2(&lines);
@@ -10,61 +10,84 @@ pub fn solve(input: &str) -> Result<DayResult> {
     (p1, p2).into_result()
 }
 
-fn solve_p1(lines: &[&[u8]]) -> i32 {
-    (0..lines[0].len())
-        .map(|x| {
-            (0..lines.len())
-                .map(|y| {
-                    [
-                        (1, 0),
-                        (-1, 0),
-                        (0, 1),
-                        (0, -1),
-                        (1, 1),
-                        (1, -1),
-                        (-1, 1),
-                        (-1, -1),
-                    ]
-                    .into_iter()
-                    .map(|(dx, dy)| check(lines, x, y, dx, dy, "XMAS") as i32)
-                    .sum::<i32>()
-                })
-                .sum::<i32>()
+struct Container<'a> {
+    source: &'a [u8],
+    width: usize,
+    height: usize,
+}
+
+impl Container<'_> {
+    fn new(source: &str) -> Result<Container> {
+        let source = source.as_bytes();
+        let Some(width) = source.iter().position(|&b| b == b'\n') else {
+            bail!("expected a newline")
+        };
+
+        let height = source.len() / (width + 1);
+
+        Ok(Container {
+            source,
+            width,
+            height,
+        })
+    }
+
+    fn get(&self, x: usize, y: usize) -> Option<u8> {
+        self.source.get(y * (self.width + 1) + x).cloned()
+    }
+}
+
+fn solve_p1(lines: &Container) -> i32 {
+    (0..lines.height)
+        .flat_map(|y| {
+            (0..lines.width).flat_map(move |x| {
+                [
+                    (1, 0),
+                    (-1, 0),
+                    (0, 1),
+                    (0, -1),
+                    (1, 1),
+                    (1, -1),
+                    (-1, 1),
+                    (-1, -1),
+                ]
+                .into_iter()
+                .map(move |(dx, dy)| check(lines, x, y, dx, dy, "XMAS") as i32)
+            })
         })
         .sum()
 }
 
-fn solve_p2(lines: &[&[u8]]) -> i32 {
-    (0..lines[0].len())
-        .map(|x| {
-            (0..lines.len())
-                .map(|y| {
-                    let leading_mas = || {
-                        check(lines, x + 2, y + 2, -1, -1, "MAS") || check(lines, x, y, 1, 1, "MAS")
-                    };
-                    let trailing_mas = || {
-                        check(lines, x + 2, y, -1, 1, "MAS") || check(lines, x, y + 2, 1, -1, "MAS")
-                    };
-
-                    (leading_mas() && trailing_mas()) as i32
-                })
-                .sum::<i32>()
+fn solve_p2(lines: &Container) -> i32 {
+    (0..lines.height)
+        .flat_map(|y| {
+            (0..lines.width).map(move |x| {
+                let leading_mas =
+                    || check(lines, x + 2, y + 2, -1, -1, "MAS") || check(lines, x, y, 1, 1, "MAS");
+                let trailing_mas =
+                    || check(lines, x + 2, y, -1, 1, "MAS") || check(lines, x, y + 2, 1, -1, "MAS");
+                (leading_mas() && trailing_mas()) as i32
+            })
         })
         .sum()
 }
 
-fn check(lines: &[&[u8]], x: usize, y: usize, dx: isize, dy: isize, s: &str) -> bool {
-    let x = x as isize;
-    let y = y as isize;
+fn check(lines: &Container, x: usize, y: usize, dx: isize, dy: isize, s: &str) -> bool {
     s.as_bytes()
         .iter()
-        .scan((x, y), |(x, y), &b| {
-            let res = lines
-                .get(*y as usize)
-                .and_then(|line| line.get(*x as usize))
-                .map(|&b2| b == b2);
-            *x += dx;
-            *y += dy;
+        .scan((true, x, y), |(cont, x, y), &b| {
+            if !*cont {
+                return Some(None);
+            }
+            let res = lines.get(*x, *y).map(|b2| b == b2);
+            match x.checked_add_signed(dx) {
+                Some(nx) => *x = nx,
+                None => *cont = false,
+            }
+            match y.checked_add_signed(dy) {
+                Some(ny) => *y = ny,
+                None => *cont = false,
+            }
             Some(res)
         })
         .all(|ok| ok.unwrap_or_default())
